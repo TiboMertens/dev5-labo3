5cadf21064c9191e38e84fb88791e34fef702396
TiboMertens <tibomertens25@gmail.com>
2023-10-06 21:21:42 +0200

    feat(lab2): get current temperature

--- src/App.js
+++ src/App.js
@@ -11,5 +11,28 @@ export default class App {
   showPosition(position) {
     let x = position.coords.latitude;
     let y = position.coords.longitude;
+    this.getWeather(x, y);
+  }
+
+  getWeather(x, y) {
+    // url: https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true&forecast_days=1
+    // fetch, then log result
+    fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=" +
+        x +
+        "&longitude=" +
+        y +
+        "&hourly=temperature_2m&current_weather=true&forecast_days=1"
+    )
+      .then((response) => response.json())
+      .then(data => {
+        let temperature = data.current_weather.temperature;
+        
+    })
+      .catch((error) => console.log(error));
+  }
+
+  showError(error) {
+    console.log(error);
   }
 }
